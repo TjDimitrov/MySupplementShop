ec00820a594d1bfaadf9d1d5306224dc50103ee1
TjDimitrov <106147047+TjDimitrov@users.noreply.github.com>
2023-10-08 13:58:32 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python ExamProject/manage.py createsuperuser --no-input
 fi
 
 python manage.py collectstatic --no-input
